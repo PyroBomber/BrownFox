47e443a87bdbf375ae59c5ee0b55eec099a4234c
PyroBomber <61275504+PyroBomber@users.noreply.github.com>
2020-02-27 23:39:35 +0700

    lvl2 update

--- lvl2.kt
+++ lvl2.kt
@@ -1,19 +1,6 @@
 fun main(args: Array<String>){
-    val n: Int = args.size
-    var temp: String
 
-    for(i in 0 until n-1)
-    {
-        for(j in 0 until n-1-i)
-        {
-            if(args[j] > args[j+1])
-            {
-                temp = args[j]
-                args[j] = args[j+1]
-                args[j+1] = temp
-            }
-        }
-    }
+    args.sort()
 
     for (i in args.indices)
         println(args[i])
