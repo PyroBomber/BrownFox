7515a2085d720b4c7bc9d872e7ef678c2ab8210d
PyroBomber <61275504+PyroBomber@users.noreply.github.com>
2020-02-28 12:51:46 +0700

    lvl3 update

--- lvl3.kt
+++ lvl3.kt
@@ -1,25 +1,7 @@
 fun main(args: Array<String>){
-    val n: Int = args.size
-    var temp: String
 
-    for(i in 0 until n-1)
-    {
-        for(j in 0 until n-1-i)
-        {
-            if(args[j] > args[j+1])
-            {
-                temp = args[j]
-                args[j] = args[j+1]
-                args[j+1] = temp
-            }
-        }
-    }
+    var words: Set<String> = args.toSortedSet()
 
-    println(args[0])
-
-    for (i in 1 until n)
-    {
-        if(args[i] != args[i-1])
-            println(args[i])
-    }
+    for(word in words)
+        println(word)
 }
